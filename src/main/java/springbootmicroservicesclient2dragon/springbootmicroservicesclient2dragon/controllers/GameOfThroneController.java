78d77ada3c4a5004f5179f7ec985eb9f5e30607f
gustavo9601 <tavo9601@gmail.com>
2022-09-11 15:45:47 -0500

    adding log

--- src/main/java/springbootmicroservicesclient2dragon/springbootmicroservicesclient2dragon/controllers/GameOfThroneController.java
+++ src/main/java/springbootmicroservicesclient2dragon/springbootmicroservicesclient2dragon/controllers/GameOfThroneController.java
@@ -1,6 +1,8 @@
 package springbootmicroservicesclient2dragon.springbootmicroservicesclient2dragon.controllers;
 
 import com.github.javafaker.Faker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,8 +23,12 @@ public class GameOfThroneController {
 
     private List<String> characters = new ArrayList<>();
 
+    // log
+    private static final Logger logger = LoggerFactory.getLogger(GameOfThroneController.class);
+
     @GetMapping
     public ResponseEntity<List<String>> getCharacters() {
+        logger.info("Ejecutando getCharacters a las :\t"+ LocalDateTime.now());
         return ResponseEntity.ok(this.characters);
     }
 
